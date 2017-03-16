8c30486a3d70b9262cd1fb8e8ae746e64feb53f3
LAPTOP-2ALLR6GQ\nhoon <nhoon2002@ucla.edu>
2017-03-15 19:41:16 -0700

    react profile

--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -25,31 +25,56 @@ class Profile extends Component {
 	render() {
 		return (
 
-        <div className='row'>
-          <div className='col-md-3'></div>
-          <div className='col-md-6'>
-            <div className='container'>
-              <h1><span>hello</span></h1>
-              {PROFILE_ITEMS.map((item, index) => {
-                return <ProfileItems
-                  key={index}
-                  fullName={item.name}
-                  description={item.description}
-                  imgSource={item.img}
-                />;
-              })}
-            </div>
+        // <div className='row'>
+        //   <div className='col-md-3'></div>
+        //   <div className='col-md-6'>
+        //     <div className='container'>
+        //       <h1><span>hello</span></h1>
+        //       {PROFILE_ITEMS.map((item, index) => {
+        //         return <ProfileItems
+        //           key={index}
+        //           fullName={item.name}
+        //           description={item.description}
+        //           imgSource={item.img}
+        //         />;
+        //       })}
+        //     </div>
+        //   </div>
+        //   <div className='col-md-3'></div>
+        //
+        //
+        //
+        // </div>
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6 holder">
+              <img className="img-circle" src="http://www.placehold.it/200x200" />
+              <hr />
+              <h2>John Doe</h2>
+              <hr />
+              <h4>Link: <a href="#">www.porfolio.com/john</a></h4>
+              <div className="icons-holder">
+                <a target='_blank' href="http://www.github.com">
+                  <img src="assets/icons/github.png" className="profIcons"/>
+                </a>
+                <a target='_blank' href="http://www.linkedin.com">
+                  <img src="assets/icons/linkedin.png" className="profIcons"/>
+                </a>
+              </div>
+              <div className="buttons-holder">
+                  <button type="button" className="btn btn-danger actionButtons">Add to Team</button>
+                  <button type="button" className="btn btn-success actionButtons">Connect</button>
+                  <button type="button" className="btn btn-primary actionButtons">Send Message</button>
+                  <button type="button" className="btn btn-warning actionButtons">Send Message</button>
+              </div>
           </div>
-          <div className='col-md-3'></div>
-
-
-
+          <div className="col-md-3"></div>
         </div>
 
 
 
     );
-  };
+  }
 }
 
 export default Profile;
